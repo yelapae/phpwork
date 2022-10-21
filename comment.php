7ad95b223c610115c065994227bbe3f29a396541
yelapae <s64309010005@ptnicec.ac.th>
2022-10-21 14:31:24 +0700

    ส่งไฟล์ test ครั้งที่1

---
+++ comment.php
@@ -0,0 +1,14 @@
+<?php
+  
+   //บรรทัดที่6-7 แสดงผลการใช้งาน Coment แบบบรรทักเดียว
+   echo "suraya Yelapae";
+   echo "Information Technology<br>";
+   //echo "Pattani<br>";
+   #echo "Thailand";
+
+   //บรรทัดที่10-12 แสดงผลการใช้งาน Coment แบบหลายบรรทัด
+   /*echo "<br>";
+   echo "<br>";
+   echo "94000<br>";
+   echo "Asia<br>"; */
+   echo "Universal";
